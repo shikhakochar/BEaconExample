26118f78ddfc374276833f991224a9f57ce50832
“shikhakochar” <shikhakochar1405@gmail.com>
2019-05-26 23:26:34 +0530

    Initial Commit

---
+++ beaconExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  beaconExample
+//
+//  Created by apple on 26/05/19.
+//  Copyright © 2019 apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
